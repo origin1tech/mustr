f37b285ab6c3172b2a330e2833b78998a4057232
origin1tech <origin1tech@gmail.com>
2017-06-26 22:55:14 -0700

    auto commit

---
+++ dist/blueprints/register.js
@@ -0,0 +1,14 @@
+
+/**
+ * See examples/register.js
+ * for detailed examples.
+ *
+ * @param mu - the Mustr instance.
+ * @param Engine - the compiler engine, Mustache by default.
+ *
+ */
+module.exports = function (mu, Engine) {
+
+  // Register templates here.
+
+};
